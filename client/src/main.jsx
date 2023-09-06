9c8aa96a5ef1291e2dc0b0d63715aab6bd6be14d
Benjamin Castro <benyamin0714@gmail.com>
2023-09-06 18:20:03 -0300

    deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import axios from "axios";
+
+const { LOCALHOST } = import.meta.env;
+axios.defaults.baseURL = LOCALHOST || "https://henry-pf-production-f816.up.railway.app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
