706379320247629d0e80fb3703879c838d6b1559
Benjamin Castro <benyamin0714@gmail.com>
2023-09-06 04:20:09 -0300

    componentes auxiliares

---
+++ client/src/components/FormPostClient.jsx
@@ -0,0 +1,115 @@
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+// eslint-disable-next-line react/prop-types
+export default function FormPostClient({ cerrarModal }) {
+  const { id } = useParams();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const { name, email, phone, status } = Object.fromEntries(formData);
+    try {
+      const response = await axios.post("http://localhost:4000/", {
+        id: id,
+        name: name,
+        email: email,
+        phone: phone,
+        status: status,
+      });
+      if (response.status === 200) cerrarModal();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="fixed flex-col z-10 bg-gray-900 flex items-center justify-center"
+    >
+      <div className="modal bg-white p-4 w-96 rounded-md shadow-md">
+        <div className="relative z-0 w-full mb-6 group">
+          <input
+            type="text"
+            name="name"
+            id="floating_email"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required=""
+          />
+          <label
+            htmlFor="floating_email"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Nombre y Apellido
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-6 group">
+          <input
+            type="email"
+            name="email"
+            id="floating_password"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required=""
+          />
+          <label
+            htmlFor="floating_password"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Correo Electronico
+          </label>
+        </div>
+        <div className="grid md:grid-cols-2 md:gap-6">
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              type="text"
+              name="phone"
+              id="floating_first_name"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required=""
+            />
+            <label
+              htmlFor="floating_first_name"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Numero de telefono
+            </label>
+          </div>
+          <div className="relative z-0 w-full mb-6 group">
+            <select
+              name="status"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required=""
+            >
+              <option className="text-gray-900" value="Off">
+                -----
+              </option>
+              <option className="text-gray-900" value="New">
+                New
+              </option>
+              <option className="text-gray-900" value="Cita">
+                Cita
+              </option>
+            </select>
+            <label
+              htmlFor="floating_first_name"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Estado
+            </label>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Enviar
+        </button>
+      </div>
+    </form>
+  );
+}
--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,70 +1,84 @@
-const Table = () => {
-  const list = [
-    {
-      client: "Benjamin castro",
-      phone: "31231231",
-      email: "XXXXXXXXXXXXXXXXXX",
-      status: "New",
-      action: "Edit",
-    },
-    {
-      client: "Benjamin castro",
-      phone: "31231231",
-      email: "XXXXXXXXXXXXXXXXXX",
-      status: "New",
-      action: "Edit",
-    },
-    {
-      client: "Benjamin castro",
-      phone: "31231231",
-      email: "XXXXXXXXXXXXXXXXXX",
-      status: "New",
-      action: "edit",
-    },
-  ];
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+// import FormPutClient from "../components/FormPutClient";
+
+// eslint-disable-next-line react/prop-types
+const Table = ({ cerrarModal }) => {
+  const [data, setData] = useState(null); // Estado para almacenar los datos de la solicitud
+  const { id } = useParams(); // Obtenemos el parámetro 'id' de la URL usando useParams
+
+  useEffect(() => {
+    // Realiza la solicitud GET al endpoint usando Axios cuando el componente se monta
+    axios
+      .get(`http://localhost:4000/${id}`)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error al obtener datos:", error);
+      });
+  }, [id]);
+
+  const handledDelete = async (id) => {
+    await axios.delete(`http://localhost:4000/${id}`);
+  };
+
+  // const handleEdit = async ({ id, name, email, phone, status }) => {
+  //   await axios.put(`http://localhost:4000/${id}`, {
+  //     name,
+  //     email,
+  //     phone,
+  //     status,
+  //   });
+  // };
+
   return (
     <>
-      {list.map((item, index) => (
-        <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-          <td
-            scope="row"
-            className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-          >
-            <input type="checkbox" className="mr-2" />
-          </td>
-          <td
-            scope="row"
-            className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-          >
-            {item.client}
-          </td>
-          <td
-            scope="row"
-            className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-          >
-            {item.phone}
-          </td>
-          <td
-            scope="row"
-            className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-          >
-            {item.email}
-          </td>
-          <td
-            scope="row"
-            className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-          >
-            {item.status}
-          </td>
-          <td
-            scope="row"
-            className="px-6 py-4 font-medium flex gap-3 text-gray-900 dark:text-white whitespace-nowrap"
-          >
-            <button>{item.action}</button>
-            <button> Delete</button>
-          </td>
-        </tr>
-      ))}
+      {/* {console.log(data)} */}
+      {data &&
+        data.data.map((item) => (
+          <tr key={item.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <td
+              scope="row"
+              className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+            >
+              <input type="checkbox" className="mr-2" />
+            </td>
+            <td
+              scope="row"
+              className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+            >
+              {item.name}
+            </td>
+            <td
+              scope="row"
+              className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+            >
+              {item.phone}
+            </td>
+            <td
+              scope="row"
+              className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+            >
+              {item.email}
+            </td>
+            <td
+              scope="row"
+              className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+            >
+              {item.status}
+            </td>
+            <td
+              scope="row"
+              className="px-6 py-4 font-medium flex gap-3 text-gray-900 dark:text-white whitespace-nowrap"
+            >
+              <button onClick={cerrarModal}>Edit</button>
+              <button onClick={() => handledDelete(item.id)}> Delete</button>
+            </td>
+          </tr>
+        ))}
+      {/* <FormPutClient /> */}
     </>
   );
 };
