a8051e95981e9aedcaf8e6fa2373bb83118ec42d
Benjamin Castro <benyamin0714@gmail.com>
2023-09-06 21:16:23 -0300

    fix deploy back

--- api/src/index.js
+++ api/src/index.js
@@ -5,7 +5,13 @@ const cors = require("cors");
 const { config } = require("dotenv");
 
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Content-Length");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.use(morgan("dev"));
 app.use(express.json());
